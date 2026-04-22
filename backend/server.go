403dd300ec029c1212ae08501d44331a8997a23c
Adam <adampattison08@gmail.com>
2026-04-22 05:11:55 +0100

    feat: create input structs

--- backend/server.go
+++ backend/server.go
@@ -11,6 +11,20 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
 
+	type SignalInput struct {
+		SignalId   int      `json:"signal_id"`
+		SignalName string   `json:"signal_name"`
+		Elr        string   `json:"elr"`
+		Mileage    *float64 `json:"mileage"`
+	}
+
+	type TrackInput struct {
+		TrackId int            `json:"track_id"`
+		Source  string         `json:"source"`
+		Target  string         `json:"target"`
+		Signals *[]SignalInput `json:"signal_ids"`
+	}
+
 	e.GET("/", func(c *echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
