4fe88cb4af8087f4f56b2f6e2dca6211f2effc02
Adam <adampattison08@gmail.com>
2026-04-22 06:41:40 +0100

    feat: install tanstack query use demo in frontend to pull in signals data, update CORS in config to allow calls to api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  server: {
+    port: 4200, // frontend port
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1323', // local address for backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
