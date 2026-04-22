40c761221033ac39579e7991eeeedad71b346eb9
Adam <adampattison08@gmail.com>
2026-04-22 07:55:23 +0100

    feat: create fetchSignalByID query

--- frontend/src/queries.ts
+++ frontend/src/queries.ts
@@ -27,3 +27,21 @@ export const fetchSignals = async (): Promise<Signal[] | null> => {
 
   return result.data;
 };
+
+export const fetchSignalByID = async (id: number): Promise<Signal | null> => {
+  const response = await fetch(`/api/signals/:${id}`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+
+  const json = await response.json();
+
+  const result = SignalSchema.safeParse(json);
+
+  if (!result.success) {
+    throw new Error(`Invalid response ${result.error}`);
+  }
+
+  return result.data;
+};
